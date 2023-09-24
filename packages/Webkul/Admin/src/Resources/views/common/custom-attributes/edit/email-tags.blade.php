77f6fd7ca5221295e7d821d0fe133940c9f13d79
gabrielxavila <gabrielxavila@gmail.com>
2023-09-23 18:11:44 -0700

    Update email-tags.blade.php

--- packages/Webkul/Admin/src/Resources/views/common/custom-attributes/edit/email-tags.blade.php
+++ packages/Webkul/Admin/src/Resources/views/common/custom-attributes/edit/email-tags.blade.php
@@ -68,7 +68,6 @@
                     },
 
                     validateEmail: function (email) {
-                        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
                         return re.test(String(email).toLowerCase());
                     }
